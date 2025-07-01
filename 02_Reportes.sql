a8ac6ae17aa1991c1f8a2e87881d659cbf28f528
Lautaro Santillan <santillanlautaro03@gmail.com>
2025-06-30 21:10:40 -0300

    Entrega 7 + nuevos files

---
+++ 02_Reportes.sql
@@ -0,0 +1,12 @@
+----------------------------------------------------------------------------------------------------
+-- Fecha de entrega: 01-07-2025
+-- Numero de grupo: 3
+-- Materia: Bases de Datos Aplicada
+-- Alumnos:
+--   - Codina, Santiago Ivan - 44.391.352
+--   - Santillan, Lautaro Ezequiel - 45.175.053
+----------------------------------------------------------------------------------------------------
+
+-- Usar la base de datos
+USE Com2900G03;
+GO
